8e15523461d93138392440277301154ed375a30f
iamjohnlloydt <trijojohnlloyd0402@gmail.com>
2023-10-03 15:55:00 +0800

    first commit

---
+++ resources/js/Components/Sidebar.vue
@@ -0,0 +1,77 @@
+<template>
+    <aside class="h-screen w-full border-r-2">
+        <div class="bg-red-2 flex items-center justify-center py-4 px-4">
+            <img src="../../css/assets/xdt-logo.png" class="w-1/2" />
+        </div>
+        <hr />
+
+        <!--Parent-->
+        <div class="px-4 w-full">
+            <ul class="space-y-5 mt-8 font-medium text-md text-[#000e31]">
+                <div class="space-x-2 flex items-center">
+                    <UserCircleIcon class="w-6 h-6" />
+                    <span>{{ $page.props.auth.user.name }}</span>
+                </div>
+            </ul>
+            <ul class="space-y-5 mt-8 font-medium text-md text-[#000e31]">
+                <span>Menu</span>
+            </ul>
+            <ul class="space-y-5 p-5 font-medium text-[#000e31] text-sm">
+                <li>
+                    <Link
+                        class="h-fit p-2 flex items-center space-x-2 hover:bg-[#E9E9E9] rounded-xl"
+                    >
+                        <ChartPieIcon class="w-6 h-6" />
+                        <span>Dashboard</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+
+                        class="h-fit p-2 flex items-center space-x-2 hover:bg-[#E9E9E9] rounded-xl"
+                    >
+                        <UserGroupIcon class="w-6 h-6" />
+                        <span>Employee</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        class="h-fit p-2 flex items-center space-x-2 hover:bg-[#E9E9E9] rounded-xl"
+                    >
+                        <CalendarDaysIcon class="w-6 h-6" />
+                        <span>Attendance</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        class="h-fit p-2 flex items-center space-x-2 hover:bg-[#E9E9E9] rounded-xl"
+                    >
+                        <BriefcaseIcon class="w-6 h-6" />
+                        <span>Leave</span>
+                    </Link>
+                </li>
+            </ul>
+            <ul class="space-y-5 mt-8 font-medium text-md text-[#000e31]">
+              <div class="flex">
+                <Link :href="route('logout')" method="post" as="button">
+                  <ArrowRightOnRectangleIcon class="w-6 h-6"/>
+                  <span>Logout</span>
+                </Link>
+              </div>
+            </ul>
+        </div>
+    </aside>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import {
+    ChartPieIcon,
+    UserGroupIcon,
+    CalendarDaysIcon,
+    BriefcaseIcon,
+    UserCircleIcon,
+    ArrowRightOnRectangleIcon,
+} from "@heroicons/vue/24/solid";
+import { Link } from "@inertiajs/vue3";
+</script>
---
+++ resources/js/Layouts/ApplicationLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import sidebar from "@/Components/Sidebar.vue";
+</script>
+
+<template>
+    <!--Header-->
+    <div>
+        <slot name="title"></slot>
+    </div>
+
+    <div class="grid max-w-full grid-cols-12">
+        <!--Side Bar-->
+        <div class="col-span-2 border-gray-300 sm:col-span-2">
+            <sidebar />
+        </div>
+
+        <!--Main Content-->
+        <div class="col-span-10 bg-[#F0F0F0] sm:col-span-10 block">
+            <slot name="body"></slot>
+        </div>
+    </div>
+    <Footer />
+</template>
